02b6e1e2966e1467f7bb0d7980fa740dc51d17bd
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-08-06 00:13:40 +0700

    Revert "Set Device Orientation."
    This reverts commit 44e0338d39cce4c88cd35aaf285503770edcde11.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:future_jobs/pages/home_page.dart';
 import 'package:future_jobs/pages/onboarding_page.dart';
 import 'package:future_jobs/pages/sign_in_page.dart';
@@ -16,11 +15,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Set Device Orientation
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      // DeviceOrientation.portraitDown,
-    ]);
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<AuthProvider>(
