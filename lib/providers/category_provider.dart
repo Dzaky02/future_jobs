c64866005b6e03ae01d6c749250e3b3117f39fb7
dzakybrori <broridzaky2702@gmail.com>
2021-10-09 22:13:06 +0700

    Fixing provider for better performance.

--- lib/providers/category_provider.dart
+++ lib/providers/category_provider.dart
@@ -1,34 +1,46 @@
+import 'dart:collection';
 import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:future_jobs/models/category_model.dart';
+
+import '../models/category_model.dart';
 
 class CategoryProvider with ChangeNotifier {
+  List<CategoryModel> _items = [];
+
+  UnmodifiableListView<CategoryModel> get items => UnmodifiableListView(_items);
+
   Future<List<CategoryModel>> getCategories() async {
-    try {
-      var response = await http.get(
-        Uri.parse('https://bwa-jobs.herokuapp.com/categories'),
-      );
+    if (_items.isEmpty) {
+      try {
+        var response = await http.get(
+          Uri.parse('https://bwa-jobs.herokuapp.com/categories'),
+        );
 
-      print(response.statusCode);
-      print(response.body);
+        print(response.statusCode);
+        print(response.body);
 
-      if (response.statusCode == 200) {
-        List<CategoryModel> categories = [];
-        List parseJson = jsonDecode(response.body);
+        if (response.statusCode == 200) {
+          List<CategoryModel> categories = [];
+          List parseJson = jsonDecode(response.body);
 
-        parseJson.forEach((category) {
-          categories.add(CategoryModel.fromJson(category));
-        });
+          parseJson.forEach((category) {
+            categories.add(CategoryModel.fromJson(category));
+          });
 
-        return categories;
-      } else {
+          _items = categories;
+          notifyListeners();
+          return UnmodifiableListView(categories);
+        } else {
+          return [];
+        }
+      } catch (e) {
+        print('ERROR Get Categories: $e');
         return [];
       }
-    } catch (e) {
-      print(e);
-      return [];
+    } else {
+      return items;
     }
   }
 }
--- lib/providers/job_provider.dart
+++ lib/providers/job_provider.dart
@@ -1,60 +1,75 @@
+import 'dart:collection';
 import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
-import 'package:future_jobs/models/job_model.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/job_model.dart';
+
 class JobProvider with ChangeNotifier {
+  List<JobModel> _jobs = [];
+  Map<String, List<JobModel>> _mapCategoriesJobs = {};
+
+  UnmodifiableListView<JobModel> get jobs => UnmodifiableListView(_jobs);
+
   Future<List<JobModel>> getJobs() async {
-    try {
-      var response = await http.get(
-        Uri.parse('https://bwa-jobs.herokuapp.com/jobs'),
-      );
+    if (_jobs.isEmpty) {
+      try {
+        var response =
+            await http.get(Uri.parse('https://bwa-jobs.herokuapp.com/jobs'));
 
-      print(response.statusCode);
-      print(response.body);
+        print(response.statusCode);
+        print(response.body);
 
-      if (response.statusCode == 200) {
-        List<JobModel> jobs = [];
-        List parsedJson = jsonDecode(response.body);
+        if (response.statusCode == 200) {
+          List<JobModel> jobs = [];
+          List parsedJson = jsonDecode(response.body);
 
-        parsedJson.forEach((job) {
-          jobs.add(JobModel.fromJson(job));
-        });
+          parsedJson.forEach((job) {
+            jobs.add(JobModel.fromJson(job));
+          });
 
-        return jobs;
+          _jobs = jobs;
+          return UnmodifiableListView(jobs);
+        }
+        return [];
+      } catch (e) {
+        print('ERROR Get Jobs: $e');
+        return [];
       }
-
-      return [];
-    } catch (e) {
-      print(e);
-      return [];
+    } else {
+      return jobs;
     }
   }
+
   Future<List<JobModel>> getJobsByCategory(String category) async {
-    try {
-      var response = await http.get(
-        Uri.parse('https://bwa-jobs.herokuapp.com/jobs?category=$category'),
-      );
+    if (!_mapCategoriesJobs.containsKey(category)) {
+      try {
+        var response = await http.get(
+          Uri.parse('https://bwa-jobs.herokuapp.com/jobs?category=$category'),
+        );
 
-      print(response.statusCode);
-      print(response.body);
+        print(response.statusCode);
+        print(response.body);
 
-      if (response.statusCode == 200) {
-        List<JobModel> jobs = [];
-        List parsedJson = jsonDecode(response.body);
+        if (response.statusCode == 200) {
+          List<JobModel> jobs = [];
+          List parsedJson = jsonDecode(response.body);
 
-        parsedJson.forEach((job) {
-          jobs.add(JobModel.fromJson(job));
-        });
+          parsedJson.forEach((job) {
+            jobs.add(JobModel.fromJson(job));
+          });
 
-        return jobs;
+          _mapCategoriesJobs.addAll({category: jobs});
+          return UnmodifiableListView(jobs);
+        }
+        return [];
+      } catch (e) {
+        print('ERROR Get Job By Category: $e');
+        return [];
       }
-
-      return [];
-    } catch (e) {
-      print(e);
-      return [];
+    } else {
+      return UnmodifiableListView(_mapCategoriesJobs[category]!);
     }
   }
 }
